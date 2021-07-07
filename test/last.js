46e18689078401ec82f0503435d9e57d9319da23
Pieter Wigboldus <w3n@w3news.org>
2021-07-07 10:29:15 +0200

    Fix the module exports, just use ES Modules only

--- test/last.js
+++ test/last.js
@@ -1,3 +1,4 @@
+import { expect, describe, it } from '@jest/globals';
 import { Arr } from '../src/helpers';
 
 describe('Last', () => {
--- test/pushMultiple.js
+++ test/pushMultiple.js
@@ -1,3 +1,4 @@
+import { expect, describe, it } from '@jest/globals';
 import { Arr } from '../src/helpers';
 
 const a = new Arr();
