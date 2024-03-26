c3b8ba1171f1e57bc8e4ab4a59c4c9c253f59429
Epeius of Phocis <epeus@w3news.org>
2024-03-26 12:56:44 +0100

    Cleanup, to start state

--- src/modules/multiply.js
+++ src/modules/multiply.js
@@ -1,10 +1,1 @@
-export default (a, b) => {
-    const result = a * b;
-    if (result > 10) {
-        return 10;
-    }
-    if (result < 1) {
-        return 1;
-    }
-    return result;
-};
+export default (a, b) => {};
--- test/multiply.js
+++ test/multiply.js
@@ -8,25 +8,4 @@ test('Multiply', async (t) => {
         const expectedResult = 9;
         assert.strictEqual(result, expectedResult);
     });
-    await t.test('It should return 4 if you send 2 and 2', () => {
-        const result = multiply(2, 2);
-        const expectedResult = 4;
-        assert.strictEqual(result, expectedResult);
-    });
-    await t.test(
-        'It should return always 10 if the result is more than 10',
-        () => {
-            const result = multiply(4, 4);
-            const expectedResult = 10;
-            assert.strictEqual(result, expectedResult);
-        }
-    );
-    await t.test(
-        'It should return always 1 if the result is less then 1',
-        () => {
-            const result = multiply(-1, 1);
-            const expectedResult = 1;
-            assert.strictEqual(result, expectedResult);
-        }
-    );
 });
