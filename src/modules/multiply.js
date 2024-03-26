e1f58a4ff2bccfede595fcf34c3932d4108dffc8
Epeius of Phocis <epeus@w3news.org>
2024-03-26 12:47:29 +0100

    Lint fixes

--- src/modules/multiply.js
+++ src/modules/multiply.js
@@ -2,9 +2,9 @@ export default (a, b) => {
     const result = a * b;
     if (result > 10) {
         return 10;
-    } else if (result < 1) {
-        return 1;
-    } else {
-        return result;
     }
-}
+    if (result < 1) {
+        return 1;
+    }
+    return result;
+};
--- test/multiply.js
+++ test/multiply.js
@@ -1,27 +1,32 @@
 import test from 'node:test';
 import assert from 'node:assert';
-import multiply from '../src/modules/multiply.js'
-
+import multiply from '../src/modules/multiply.js';
 
 test('Multiply', async (t) => {
     await t.test('It should return 9 if you send 3 and 3', () => {
         const result = multiply(3, 3);
         const expectedResult = 9;
         assert.strictEqual(result, expectedResult);
-    })
+    });
     await t.test('It should return 4 if you send 2 and 2', () => {
         const result = multiply(2, 2);
         const expectedResult = 4;
         assert.strictEqual(result, expectedResult);
-    })
-    await t.test('It should return always 10 if the result is more than 10', () => {
-        const result = multiply(4, 4);
-        const expectedResult = 10;
-        assert.strictEqual(result, expectedResult);
-    })
-    await t.test('It should return always 1 if the result is less then 1', () => {
-        const result = multiply(-1, 1);
-        const expectedResult = 1;
-        assert.strictEqual(result, expectedResult);
-    })
+    });
+    await t.test(
+        'It should return always 10 if the result is more than 10',
+        () => {
+            const result = multiply(4, 4);
+            const expectedResult = 10;
+            assert.strictEqual(result, expectedResult);
+        }
+    );
+    await t.test(
+        'It should return always 1 if the result is less then 1',
+        () => {
+            const result = multiply(-1, 1);
+            const expectedResult = 1;
+            assert.strictEqual(result, expectedResult);
+        }
+    );
 });
